093ec6e9753ea99218edf6596778d010570c237a
manosbatsis <manosbatsis@gmail.com>
2021-08-09 11:24:34 +0300

    chore(misc): minor fixes

--- corda-statemeant/src/main/kotlin/com/github/manosbatsis/corda/statemeant/annotation/StatemeantType.kt
+++ corda-statemeant/src/main/kotlin/com/github/manosbatsis/corda/statemeant/annotation/StatemeantType.kt
@@ -17,6 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
  * USA
  */
+
 /*
  * Corda Statemeant: Generate Corda Contract and Persistent states
  * from a simplified model interface
